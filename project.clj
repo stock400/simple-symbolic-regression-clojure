7bc9948cb0227abe39f7d96af864fbca439f42d6
Nic McPhee <mcphee@morris.umn.edu>
2015-08-16 01:01:54 -0500

    Added a Rubric record and and `error-on` method.

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
 (defproject simple-symbolic-regression-clojure "0.0.1-SNAPSHOT"
   :description "Cool new project to do things and stuff"
-  :dependencies [[org.clojure/clojure "1.7.0"] [bigml/sampling "3.0"]]
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [org.clojure/math.numeric-tower "0.0.4"]]
   :profiles {:dev {:dependencies [[midje "1.7.0"]]}})
 
