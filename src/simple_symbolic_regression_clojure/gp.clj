fa8e60f068def9c9a3c276ce624088c4a69e0333
Bill Tozier <bill@vagueinnovation.com>
2015-08-16 20:27:20 -0400

    uniform crossover, point mutation, random token generators and one-point uneven crossover

--- src/simple_symbolic_regression_clojure/gp.clj
+++ src/simple_symbolic_regression_clojure/gp.clj
@@ -1,1 +1,37 @@
-(ns simple-symbolic-regression-clojure.gp)
+(ns simple-symbolic-regression-clojure.gp
+  )
+
+
+(defn random-token
+  "Argument is a list of tokens, including functions to generate values if you like;
+  every token is evaluated as it is returned, and they are sampled with uniform probability."
+  [tokens]
+  (eval (nth tokens (rand-int (count tokens))))
+  )
+
+
+(defn uniform-crossover
+  "Takes two collections, and returns a new collection containing items taken with
+  equal probability from the two 'parents' at each location. The length of the
+  crossover-product is the shorter of the two lengths."
+  [mom dad]
+  (map (fn [a b] (if (< 0.5 (rand)) a b)) mom dad)
+  )
+
+(defn uniform-mutation
+  "Takes a collections, a vector of tokens (including functions), and a probability;
+  resamples eachposition with i.i.d. probability, using the token list to replace them.
+  The result will not change length."
+  [mom tokens prob]
+  (map (fn [i] (if (< (rand) prob) (random-token tokens) i)) mom)
+  )
+
+(defn one-point-crossover
+  "Takes two collections, and returns a new collection containing items taken from the
+  front of the first and the tail of the second, switching at a randomly-chosen breakpoint
+  in each."
+  [mom dad]
+  (let [mom-cut (rand-int (count mom))
+        dad-cut (rand-int (count dad))]
+    (concat (take mom-cut mom) (drop dad-cut dad)) 
+  ))
--- test/simple_symbolic_regression_clojure/gp_test.clj
+++ test/simple_symbolic_regression_clojure/gp_test.clj
@@ -1,8 +1,25 @@
 (ns simple-symbolic-regression-clojure.gp-test
   (:use midje.sweet)
   (:use [simple-symbolic-regression-clojure.gp])
-  (:require (bigml.sampling [simple :as simple]))
+  (:use (bigml.sampling [simple :as simple]))
   )
+
+;; helpers for testing
+
+(defn cycler
+  [numbers]
+  (atom {:step 0, :values (cycle numbers)})
+  )
+
+(defn step-cycler
+  [c]
+  (let [result (nth (:values @c) (:step @c))]
+    (do
+      (swap! c assoc :step (inc (:step @c)))
+      result
+  )))
+
+;; a bit of exploration on how to test stochastic functions
 
 (fact "we can select a subsample of a collection using bigml.sampling"
   (count (take 5 (simple/sample (range 1000)))) => 5
@@ -19,19 +36,6 @@
     [(rand-int 11) (rand-int 22) (rand-int 33)] => (just [88 88 88]))
   )
 
-(defn cycler
-  [numbers]
-  (atom {:step 0, :values (cycle numbers)})
-  )
-
-(defn step-cycler
-  [c]
-  (let [result (nth (:values @c) (:step @c))]
-    (do
-      (swap! c assoc :step (inc (:step @c)))
-      result
-  )))
-
 (fact "we can call step-cycler to produce a number from the cycler"
   (let [cc (cycler [-1 2 -3 4])]
     (step-cycler cc) => -1
@@ -40,7 +44,7 @@
     (step-cycler cc) => 4
     ))
 
-(fact "we can NOW stub random number generation with a cycler"
+(fact "we can thus stub random number generation with a cycler"
   (let [stubby (cycler [8 7 2 5])]
     (with-redefs [rand-int (fn [arg] (step-cycler stubby))]
       (take 7 (repeatedly #(rand-int 1000))) => (just [8 7 2 5 8 7 2])))
@@ -50,4 +54,48 @@
   (let [stubby (cycler [0.8 0.7 0.2 0.5])]
     (with-redefs [rand (fn [arg] (step-cycler stubby))]
       (take 7 (repeatedly #(rand 9.0))) => (just [0.8 0.7 0.2 0.5 0.8 0.7 0.2])))
+  )
+
+;; random token
+
+(fact "random-token produces a random token, including ERCs if told to"
+    (random-token [1]) => 1
+    (into #{} (repeatedly 100 #(random-token [1 2 3 4]))) => (just [1 2 3 4])
+    (into #{} (repeatedly 100 #(random-token ['(rand-int 5)]))) => (just [0 1 2 3 4])
+    (into #{} (repeatedly 100 #(random-token [:x :+ '(rand-int 5)]))) => (just [0 1 2 3 4 :x :+])
+  )
+
+;; uniform crossover
+
+(fact "uniform crossover takes two collections and picks the value at each position with equal probability from each parent"
+  (let [stubby (cycler [0.0 1.0])] ;; start with mom, alternate with dad
+    (with-redefs [rand (fn [] (step-cycler stubby))]
+      (uniform-crossover [1 1 1 1 1 1 1 1 1 1] [2 2 2 2 2 2 2 2 2]) => (just [2 1 2 1 2 1 2 1 2])))
+  )
+
+(fact "uniform crossover works with different-length collections"
+  (count (uniform-crossover [1 1 1 1 1] [2 2 2 2 2 2 2 2 2])) => 5
+  (count (uniform-crossover [1 1 1 1 1] [2])) => 1
+  )
+
+;; uniform mutation
+
+(fact "uniform mutation takes a collection, and changes each position to a new sampled value with specified probability"
+    (uniform-mutation [1 1 1] [4] 1.0) => (just [4 4 4])
+    (uniform-mutation [1 1 1] [4] 0.0) => (just [1 1 1])
+    (into #{} (uniform-mutation [1 1 1 1 1 1 1 1 1 1] [4 5] 1.0)) => (just #{4 5})
+    (into #{} (uniform-mutation [1 1 1 1 1 1 1 1 1 1] ['(+ 9 (rand-int 2))] 1.0)) => (just #{9 10})
+  )
+
+
+;; uneven one-point-crossover
+
+(fact "one-point crossover takes two collections and splices them together at a randomly chosen internal point"
+  (let [stubby (cycler [3 3 2 4 1 5 7 0])] ;; these will be used in pairs to pick slice points
+    (with-redefs [rand-int (fn [arg] (step-cycler stubby))]
+      (one-point-crossover [1 1 1 1 1 1 1 1] [2 2 2 2 2 2 2 2]) => (just [1 1 1 2 2 2 2 2])
+      (one-point-crossover [1 1 1 1 1 1 1 1] [2 2 2 2 2 2 2 2]) => (just [1 1 2 2 2 2])
+      (one-point-crossover [1 1 1 1 1 1 1 1] [2 2 2 2 2 2 2 2]) => (just [1 2 2 2])
+      (one-point-crossover [1 1 1 1 1 1 1 1] [2 2 2 2 2 2 2 2]) => (just [1 1 1 1 1 1 1 2 2 2 2 2 2 2 2])
+      )) 
   )
