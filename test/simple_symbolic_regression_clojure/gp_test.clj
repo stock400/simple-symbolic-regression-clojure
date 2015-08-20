12c7db4b7fc8e71a25d2787d60b0cbef862888c2
Bill Tozier <bill@vagueinnovation.com>
2015-08-20 14:36:12 -0400

    removed some design spike stuff

--- test/simple_symbolic_regression_clojure/gp_test.clj
+++ test/simple_symbolic_regression_clojure/gp_test.clj
@@ -177,15 +177,3 @@
   ))
 
 
-;; exploratory
-
-(def population 
-  (atom (into #{} 
-    (repeatedly 10 
-      #(make-individual (random-script ['(rand-int 100) '(rand) '(rand-int 1000) '(rand-int 10) :+ :- :* :÷ :x] 10) nil))
-    )))
-
-; (def x-plus-6-rubrics 
-;   (repeatedly 20 #(let [x (rand-int 100)] (Rubric. {:x x} (+ 6 x)))))
-
-; (println x-plus-6-rubrics)
