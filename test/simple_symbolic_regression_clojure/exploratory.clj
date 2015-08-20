6c765cbd88ac24d7fc2318ace5c1ef4494077316
Bill Tozier <bill@vagueinnovation.com>
2015-08-20 17:32:22 -0400

    fixed an inadvertent bug in exploratory.clj

--- test/simple_symbolic_regression_clojure/exploratory.clj
+++ test/simple_symbolic_regression_clojure/exploratory.clj
@@ -11,23 +11,24 @@
   ['(rand-int 100) '(rand) '(rand-int 1000) '(rand-int 10) :+ :- :* :÷ :x])
 
 (def x-plus-6-rubrics 
-  (map #(->Rubric {:x %} (+ 6 %)) (range 20)))
+  (map #(->Rubric {:x %} (+ 1963 (* 6 %) (* 25 % %))) (range 20)))
 
 (def population 
   (atom (into [] 
-    (repeatedly 50 
+    (repeatedly 100 
       #(make-individual (random-script random-code-atoms
-        5) nil))
+        100) nil))
     )))
-
-(swap! population #(into [] (map (partial score-individual x-plus-6-rubrics) %)))
-
-;; the preceding 2 lines should be folded into a single function make-population
 
 (defn score-individual
   [rubrics individual]
   (set-score individual (total-score-on (:script individual) rubrics))
   individual)
+
+(swap! population #(into [] (map (partial score-individual x-plus-6-rubrics) %)))
+
+;; the preceding 3 lines should be folded into a single function make-population
+
 
 
 ; (println population)
@@ -65,12 +66,12 @@
   [population]
   (swap! population conj (mutant
     (crossover-baby
-      (first (tournament-of-n 2 @population))
+      (first (tournament-of-n 5 @population))
       (first (tournament-of-n 7 @population)))
-    0.3)))
+    0.1)))
 
 ;; put it through its paces:
-(time (doall (repeatedly 1000 #(step population))))
+(time (doall (repeatedly 10000 #(step population))))
 
 ;; print the scores
 (println (reverse (sort (map get-score @population))))
