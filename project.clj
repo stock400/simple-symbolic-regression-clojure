6d8583ae3de7e577effcc13cf013a6e655be154d
Nic McPhee <mcphee@morris.umn.edu>
2015-09-15 08:53:26 -0500

    Added different garbage collector and more memory.

--- project.clj
+++ project.clj
@@ -3,9 +3,12 @@
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/math.numeric-tower "0.0.4"]
                  [com.climate/claypoole "1.1.0"]]
-  :profiles {:dev {:dependencies [[midje "1.7.0"][criterium "0.4.3"]]}}
+  :profiles {:dev {:dependencies [[midje "1.7.0"]
+                                  [lein-midje "3.1.1"]
+                                  [criterium "0.4.3"]]}}
   :main simple-symbolic-regression-clojure.core
-  :jvm-opts ["-Xmx1g"
+  :jvm-opts ["-Xms1g" "-Xmx4g"
+             "-XX:+UseG1GC"
              "-Dcom.sun.management.jmxremote"
              "-Dcom.sun.management.jmxremote.ssl=false"
              "-Dcom.sun.management.jmxremote.authenticate=false"
