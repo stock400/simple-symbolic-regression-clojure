8cf4ae27d9b703f9050c8dbc5a118a095d958d97
Nic McPhee <mcphee@morris.umn.edu>
2015-09-15 03:32:37 -0500

    Added a description, dependencies on a few new packages, a specification of a `-main` method, and some JVM settings.
    The dependencies on `calliope` and `criterium` might never actually be used, in which we should probably remove them later.
    
    The `-Xmx1g` JVM option sets the max heap size at 1Gb to reduce thrashing with large populations, although I'm not sure it helps much. The other four set up things so you can use JVisualVM to do profiling on the system while it's running.

--- project.clj
+++ project.clj
@@ -1,6 +1,13 @@
 (defproject simple-symbolic-regression-clojure "0.0.1-SNAPSHOT"
-  :description "Cool new project to do things and stuff"
+  :description "Simple stack-based GP system"
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.clojure/math.numeric-tower "0.0.4"]]
-  :profiles {:dev {:dependencies [[midje "1.7.0"]]}})
+                 [org.clojure/math.numeric-tower "0.0.4"]
+                 [com.climate/claypoole "1.1.0"]]
+  :profiles {:dev {:dependencies [[midje "1.7.0"][criterium "0.4.3"]]}}
+  :main simple-symbolic-regression-clojure.core
+  :jvm-opts ["-Xmx1g"
+             "-Dcom.sun.management.jmxremote"
+             "-Dcom.sun.management.jmxremote.ssl=false"
+             "-Dcom.sun.management.jmxremote.authenticate=false"
+             "-Dcom.sun.management.jmxremote.port=43210"])
 
