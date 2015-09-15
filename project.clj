9d7fe268775e7f6f1e60cff9ba309fef5534fd6d
Nic McPhee <mcphee@morris.umn.edu>
2015-09-15 10:09:27 -0500

    Removed some unused dependencies.

--- project.clj
+++ project.clj
@@ -1,11 +1,9 @@
 (defproject simple-symbolic-regression-clojure "0.0.1-SNAPSHOT"
   :description "Simple stack-based GP system"
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.clojure/math.numeric-tower "0.0.4"]
-                 [com.climate/claypoole "1.1.0"]]
+                 [org.clojure/math.numeric-tower "0.0.4"]]
   :profiles {:dev {:dependencies [[midje "1.7.0"]
-                                  [lein-midje "3.1.3"]
-                                  [criterium "0.4.3"]]}}
+                                  [lein-midje "3.1.3"]]}}
   :main simple-symbolic-regression-clojure.core
   :jvm-opts ["-Xms1g" "-Xmx4g"
              "-XX:+UseG1GC"
